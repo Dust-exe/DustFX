9281fa1ee3ad7e373a5730aeac62ec461f4767ce
dust.exe <dustexee@gmail.com>
2026-09-01 20:28:50 +0000

    v3.0.2: Static link libgcc/libstdc++ — no DLL dependency

--- build-win/CTestTestfile.cmake
+++ build-win/CTestTestfile.cmake
@@ -5,6 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[HTTP_Server_Tests]=] "/root/DustFX/build-win/bin/DustFX_Tests.exe")
-set_tests_properties([=[HTTP_Server_Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/root/DustFX/CMakeLists.txt;75;add_test;/root/DustFX/CMakeLists.txt;0;")
+set_tests_properties([=[HTTP_Server_Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/root/DustFX/CMakeLists.txt;80;add_test;/root/DustFX/CMakeLists.txt;0;")
 add_test([=[Settings_Manager_Tests]=] "/root/DustFX/build-win/bin/DustFX_Settings_Tests.exe")
-set_tests_properties([=[Settings_Manager_Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/root/DustFX/CMakeLists.txt;81;add_test;/root/DustFX/CMakeLists.txt;0;")
+set_tests_properties([=[Settings_Manager_Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/root/DustFX/CMakeLists.txt;86;add_test;/root/DustFX/CMakeLists.txt;0;")
